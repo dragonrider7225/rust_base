71ea6d3a157103040f633eec00a64a01abe47e8e
Kevin Moonen <dragonrider7225@gmail.com>
2023-01-16 02:51:43 -0600

    Add lint to deny todos unless debug assertions are enabled

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@
 #![warn(clippy::all)]
 #![warn(missing_copy_implementations, missing_docs, rust_2018_idioms)]
 #![deny(unsafe_op_in_unsafe_fn, missing_debug_implementations)]
+#![cfg_attr(not(debug_assertions), deny(clippy::todo))]
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![warn(clippy::all)]
 #![warn(missing_copy_implementations, missing_docs, rust_2018_idioms)]
 #![deny(unsafe_op_in_unsafe_fn, missing_debug_implementations)]
+#![cfg_attr(not(debug_assertions), deny(clippy::todo))]
 
 fn main() {
     println!("Hello, world!");
