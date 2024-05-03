5d85269552c793fe2034e6ed5acfd8d43905aa31
Kevin Moonen <dragonrider7225@gmail.com>
2024-05-03 01:48:50 -0500

    Warn on some default-allow clippy lints
    Lints that are always enabled:
    
    * create_dir
    * infinite_loop
    * let_underscore_must_use
    * missing_panics_doc
    * return_self_not_must_use
    * same_name_method
    * should_panic_without_expect
    * use_debug
    * missing_docs_in_private_items
    
    Lints that are enabled in release builds:
    
    * dbg_macro
    * unimplemented
    
    Lints that are enabled when the "lint_reasons" feature is enabled:
    * allow_attributes
    * allow_attributes_without_reason

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 //! My base for executable programs.
 //! TODO: Fix crate name in Cargo.toml
 
-#![warn(missing_copy_implementations, missing_docs, rust_2018_idioms)]
+#![warn(
+    clippy::create_dir,
+    clippy::infinite_loop,
+    clippy::let_underscore_must_use,
+    clippy::missing_panics_doc,
+    clippy::return_self_not_must_use,
+    clippy::same_name_method,
+    clippy::should_panic_without_expect,
+    clippy::use_debug,
+    missing_copy_implementations,
+    rust_2018_idioms
+)]
+#![warn(clippy::missing_docs_in_private_items, missing_docs)]
 #![deny(unsafe_op_in_unsafe_fn, missing_debug_implementations)]
-#![cfg_attr(not(debug_assertions), deny(clippy::todo))]
+#![cfg_attr(
+    not(debug_assertions),
+    deny(clippy::dbg_macro, clippy::todo, clippy::unimplemented)
+)]
+#![cfg_attr(
+    feature = "lint_reasons",
+    deny(clippy::allow_attributes, clippy::allow_attributes_without_reason)
+)]
 
 fn main() {
     println!("Hello, world!");
